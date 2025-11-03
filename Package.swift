03e2ec36dc4e97c76b43de3ac478ec087224fe08
Timotius Leonardo <lionato14@gmail.com>
2025-11-03 22:03:26 +0700

    Chore: Add SDWebImage as depedencies

--- Package.swift
+++ Package.swift
@@ -17,14 +17,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.1.0")
+        .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.1.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", .upToNextMajor(from: "5.21.3")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SDWebImageWebPCoder",
-            dependencies: ["libwebp"],
+            dependencies: ["libwebp", "SDWebImage"],
             path: ".",
             sources: ["SDWebImageWebPCoder/Classes"],
             publicHeadersPath: "SDWebImageWebPCoder/Classes",
